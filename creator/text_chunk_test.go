fdb039efa45f2ecb65824bd6fa1d86139a381394
Joice M. Joseph <joice@moolekkari.net>
2021-01-18 09:37:12 +0530

    removed ci tools, some syntactic sugar in test case

--- creator/text_chunk_test.go
+++ creator/text_chunk_test.go
@@ -88,19 +88,19 @@ func TestTextChunkFit(t *testing.T) {
 	})
 
 	expected := [][2]string{
-		[2]string{
+		{
 			"Lorem ipsum dolor sit",
 			"amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 		},
-		[2]string{
+		{
 			"amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et",
 			"dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 		},
-		[2]string{
+		{
 			"dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in",
 			"reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 		},
-		[2]string{
+		{
 			"reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 			"",
 		},
